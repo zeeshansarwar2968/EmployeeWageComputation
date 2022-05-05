f4c9a4da5f148a52d50ae6439d825368717ae6e5
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-05 15:43:44 +0530

    First commit of program to master branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
