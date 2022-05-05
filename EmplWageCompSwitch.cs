efd07333880f301a1d7d3b8a0afc7eabf6507f58
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-05 17:48:51 +0530

    Added logic to calculate wages for a month

--- EmplWageCompSwitch.cs
+++ EmplWageCompSwitch.cs
@@ -12,34 +12,43 @@ namespace EmployeeWageComputation
         const int isFullTime = 1;
         const int isPartTime = 2;
         const int emplHourlyRate = 20;
+        const int totalWorkingDays = 20;
 
         int emplHours = 0;
         int emplWage = 0;
+        int totalEmplWage = 0;
 
         Random NumRandom = new Random();
 
-        //Method containing logic to find the wage of an employee if they are fulltime/parttime
+        //Method containing logic to find the wage of an employee for the duration of a month
         public void CalculateEmployeeWage()
         {
-            int num = NumRandom.Next(0, 3);
-            Console.WriteLine(num);
-
-            switch (num)
+            for(int i = 0; i < totalWorkingDays; i++)
             {
-                case isFullTime:
-                    emplHours = 8;
-                    break;
+                int num = NumRandom.Next(0, 3);
+                //Console.WriteLine(num);
 
-                case isPartTime:
-                    emplHours = 4;
-                    break;
+                switch (num)
+                {
+                    case isFullTime:
+                        emplHours = 8;
+                        break;
 
-                default:
-                    emplHours = 0;
-                    break;
+                    case isPartTime:
+                        emplHours = 4;
+                        break;
+
+                    default:
+                        emplHours = 0;
+                        break;
+                }
+                emplWage = emplHours * emplHourlyRate;
+                totalEmplWage = totalEmplWage + emplWage;
+                Console.WriteLine("Day: {0}",i+1);
+                Console.WriteLine("Daily Employee Wage : "+emplWage);
+                Console.WriteLine("");
             }
-            emplWage = emplHours * emplHourlyRate;
-            Console.WriteLine("THe Wage of the Employee is : {0}", emplWage);
+            Console.WriteLine("THe Total Monthly Wage of the Employee is : {0}", totalEmplWage);
         }
     }
 }
